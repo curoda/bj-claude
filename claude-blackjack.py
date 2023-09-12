6cab2fbef11a3fb83bd3865ebe52cf785b284597
Curt Odar <curoda@users.noreply.github.com>
2023-09-11 20:31:25 -0400

    Update claude-blackjack.py

--- claude-blackjack.py
+++ claude-blackjack.py
@@ -1,23 +1,23 @@
-import random 
+import random
 import streamlit as st
 
-# Card class 
-from collections import namedtuple
+from collections import namedtuple  
+
 Card = namedtuple('Card', ['rank', 'suit'])
 
 ranks = list(range(2, 11)) + ['J', 'Q', 'K', 'A']
 suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs']
 
 values = {rank: rank for rank in ranks[:-1]}
-values['J'] = 10 
-values['Q'] = 10
+values['J'] = 10
+values['Q'] = 10  
 values['K'] = 10
 values['A'] = 11
 
 class Deck:
 
     def __init__(self):
-        self.cards = [Card(suit, rank) for suit in suits for rank in ranks]
+        self.cards = [Card(s, r) for s in suits for r in ranks]
         
     def shuffle(self):
         random.shuffle(self.cards)
@@ -25,7 +25,7 @@ class Deck:
     def deal(self):
         return self.cards.pop()
 
-
+    
 class Hand:
     
     def __init__(self):
@@ -33,55 +33,51 @@ class Hand:
         self.value = 0
         
     def add_card(self, card):
+        rank = card.rank # Added this
         self.cards.append(card)
-        self.value += values[card.rank]
-        if card.rank == 'A' and self.value > 21:
+        self.value += values[rank]
+        if rank == 'A' and self.value > 21:
             self.value -= 10
 
+
 def hit(deck, hand):
-    card = deck.deal()
-    hand.add_card(card)
+    hand.add_card(deck.deal())
 
+    
 def play_round(deck):
-    player_hand = Hand()
-    dealer_hand = Hand()
+    player = Hand()
+    dealer = Hand()
     
-    # Initial deal
-    hit(deck, player_hand)
-    hit(deck, dealer_hand)
-    hit(deck, player_hand)
-    hit(deck, dealer_hand)
-    
-    # Player turn
-    while player_hand.value < 21:
-        hit(deck, player_hand)
-        if player_hand.value > 21:
-            return -1
-            
-    # Dealer turn 
-    while dealer_hand.value < 17:
-        hit(deck, dealer_hand)
-        if dealer_hand.value > 21:
-            return 1
-            
-    return 1 if player_hand.value > dealer_hand.value else -1
-    
-        
-def run_simulations(n):
-    deck = Deck()
-    deck.shuffle()
+    hit(deck, player)
+    hit(deck, dealer)
+    hit(deck, player)
+    hit(deck, dealer)
 
+    while player.value < 21:
+        hit(deck, player)
+        if player.value > 21:
+            return -1
+
+    while dealer.value < 17:
+        hit(deck, dealer)
+        if dealer.value > 21:
+            return 1
+    
+    return 1 if player.value > dealer.value else -1
+
+    
+def run_simulations(n):
     wins = 0
     for _ in range(n):
-        result = play_round(deck)
-        if result > 0:
+        deck = Deck()
+        deck.shuffle()
+        if play_round(deck) > 0:
             wins += 1
+    return wins / n
 
-    win_rate = wins / n
-    return win_rate
 
-st.title('Blackjack Simulation')
-num_hands = st.number_input('Number of hands', min_value=10)
+st.title('Blackjack Simulation')  
+num_hands = st.number_input('Number hands', min_value=10)
 win_rate = run_simulations(num_hands)
 
 st.write(f'Win rate: {win_rate:.2%}')
